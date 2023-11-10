373fe5f527ca87aa9a3a4f94469ee51b1058f53a
Rastko <rastko.strbac@gmail.com>
2023-11-10 22:35:18 +0100

    Make function play
    Function has two parameters player and computer choice. It has a if
    condition which returns different values (from 0 to 2(inclusive)). The
    if condition compares player choice "Rock" with all computer options and
    returns values based on computer choice.

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function getComputerChoice(){
        return "SCISSORS";
    }
 }
+
+function play(playerSelection, computerSelection){
+   playerSelection = prompt("Rock, Paper or Scissors?");
+   playerSelection = playerSelection.toUpperCase();
+   computerSelection = getComputerChoice();
+   if(playerSelection === "ROCK"){
+       if(computerSelection === "ROCK"){
+           console.log("It's a draw! Rock vs Rock.");
+           return 0;
+       }
+       else if(computerSelection === "PAPER"){
+           console.log("You lose! Paper beats Rock.");
+           return 2;
+       }
+       else if(computerSelection === "SCISSORS"){
+           console.log("You win! Rock beats Scissors.");
+           return 1;
+       }
+   }
+}
